260bc34cde7d84ccb9f45d30339c1e1764af18c0
Vaz <the4wonz@outlook.com>
2022-09-17 08:38:47 +0700

    Update util.py

--- util.py
+++ util.py
@@ -4,7 +4,7 @@ import talib as ta
 import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
-import math 
+import math
 import multiprocessing as mp
 #VXMA VAZ
 #standard TA weighing
@@ -24,7 +24,7 @@ def callbackRate(data):
             rate = 5
         elif rate < 0.1 :
             rate = 0.1
-        return rate 
+        return rate
     except Exception as e:
         print(f'callbackRate is error : {e}')
         return 2.5
@@ -36,7 +36,7 @@ def swingL(data, Pivot):
     except Exception as e:
         print(f'swing Low is error : {e}')
         return data
-    
+
 def swingH(data, Pivot):
     try:
         data['Highest'] = ta.MAX(data['High'], int(Pivot))
@@ -44,9 +44,11 @@ def swingH(data, Pivot):
     except Exception as e:
         print(f'swing High is error : {e}')
         return data
-    
+
 def andean(close, open, aol_ip, m):
     alpha = 2/(int(aol_ip) + 1)
+    Close = pd.Series(close, dtype=np.float64)
+    Open = pd.Series(open, dtype=np.float64)
     up2 = np.full(m, np.nan)
     up1 = np.full(m, np.nan)
     dn1 = np.full(m, np.nan)
@@ -55,18 +57,18 @@ def andean(close, open, aol_ip, m):
     cBear = np.full(m, np.nan)
     try:
         for i in range(2,m):
-            up11 = (max(close[i],open[i],up1[i-1] - (up1[i-1] - close[i])*alpha))
-            up22 = (max(close[i]*close[i],open[i]*open[i],up2[i-1] - (up2[i-1] - close[i]*close[i])*alpha))
-            dn11 = (min(close[i],open[i],dn1[i-1] + (close[i] - dn1[i-1])*alpha))
-            dn22 = (min(close[i]*close[i],open[i]*open[i],dn2[i-1] + (close[i]*close[i] - dn2[i-1])*alpha))
+            up11 = (max(Close[i],Open[i],up1[i-1] - (up1[i-1] - Close[i])*alpha))
+            up22 = (max(Close[i]*Close[i],Open[i]*Open[i],up2[i-1] - (up2[i-1] - Close[i]*Close[i])*alpha))
+            dn11 = (min(Close[i],Open[i],dn1[i-1] + (Close[i] - dn1[i-1])*alpha))
+            dn22 = (min(Close[i]*Close[i],Open[i]*Open[i],dn2[i-1] + (Close[i]*Close[i] - dn2[i-1])*alpha))
             # up1[i-1] := nz(math.max(C, O, up1[1] - (up1[1] - C) * alpha), C)
-            up11 = up1[i] = up11 if up22 is not np.nan else close[i]
+            up11 = up1[i] = up11 if up22 is not np.nan else Close[i]
             # up2[i-1] := nz(math.max(C * C, O * O, up2[1] - (up2[1] - C * C) * alpha), C * C)
-            up22 = up2[i] = up22 if up22 is not np.nan else close[i]*close[i]
+            up22 = up2[i] = up22 if up22 is not np.nan else Close[i]*Close[i]
             # dn1[i-1] := nz(math.min(C, O, dn1[1] + (C - dn1[1]) * alpha), C)
-            dn11 = dn1[i] = dn11 if dn11 is not np.nan else close[i]
+            dn11 = dn1[i] = dn11 if dn11 is not np.nan else Close[i]
             # dn2[i-1] := nz(math.min(C * C, O * O, dn2[1] + (C * C - dn2[1]) * alpha), C * C)
-            dn22 = dn2[i] = dn22 if dn22 is not np.nan else close[i]*close[i]
+            dn22 = dn2[i] = dn22 if dn22 is not np.nan else Close[i]*Close[i]
             cBull[i] = math.sqrt(dn22 - (dn11 * dn11))
             cBear[i] = math.sqrt(up22 - (up11 * up11))
         return cBull, cBear
@@ -75,12 +77,16 @@ def andean(close, open, aol_ip, m):
         return cBull, cBear
 
 #AlphaTrend
-def alPha(data, atr_input, atrM_input, rsi_ip, m):
+def alPhaT(data, atr_input, atrM_input, rsi_ip, m):
     alpha = np.full(m, np.nan)
-    atr = ta.ATR(data['High'],data['Low'],data['Close'], int(atr_input))
-    rsi = ta.RSI(data['Close'], int(rsi_ip))
-    dnT = pd.Series(data['High']+ atr * float(atrM_input), dtype=np.float64) 
-    upT = pd.Series(data['Low'] - atr * float(atrM_input), dtype=np.float64) 
+    Close = pd.Series(data['Close'], dtype=np.float64)
+    Open = pd.Series(data['Open'], dtype=np.float64)
+    High = pd.Series(data['High'], dtype=np.float64)
+    Low = pd.Series(data['Low'], dtype=np.float64)
+    atr = ta.ATR(High,Low,Close, int(atr_input))
+    rsi = ta.RSI(Close, int(rsi_ip))
+    dnT = pd.Series(High + atr * float(atrM_input))
+    upT = pd.Series(Low - atr * float(atrM_input))
     #alPha rsibb >= 50 ? upT < nz(alPha[1]) ? nz(alPha[1]) : upT : downT > nz(alPha[1]) ? nz(alPha[1]) : downT
     try:
         for i in range(1,m):
@@ -96,18 +102,17 @@ def alPha(data, atr_input, atrM_input, rsi_ip, m):
     except Exception as e:
         print(f'AlphaTrend is error : {e}')
         return alpha
-    
+
 #VXMA
-def VXMA(data, ema_ip, subhag, smooth, m):
+def VXMA(data, ema_ip, subhag, smooth, atr_input, atrM_input, rsi_ip,aol_ip, m):
     vxma = np.full(m, np.nan)
     EMAFAST = ta.EMA(data['Close'], int(ema_ip))
-    LINREG = ta.EMA(ta.LINEARREG(data['Close'], int(subhag)), int(smooth))  
-    alPha = pd.Series(data['alPha'], dtype=np.float64)
-    cBull = pd.Series(data['cBull'], dtype=np.float64)
-    cBear = pd.Series(data['cBear'], dtype=np.float64)
+    LINREG = ta.EMA(ta.LINEARREG(data['Close'], int(subhag)), int(smooth))
+    alPha = alPhaT(data, atr_input, atrM_input, rsi_ip, m)
+    cBull, cBear = andean(data['Close'], data['Open'], aol_ip, m)
     #alPha rsibb >= 50 ? upT < nz(alPha[1]) ? nz(alPha[1]) : upT : downT > nz(alPha[1]) ? nz(alPha[1]) : downT
     try:
-        for i in range(2,m): 
+        for i in range(2,m):
             clohi = max(EMAFAST[i],LINREG[i],alPha[i-2])
             clolo = min(EMAFAST[i],LINREG[i],alPha[i-2])
                 #CloudMA := (bull > bear) ? clolo < nz(CloudMA[1]) ? nz(CloudMA[1]) : clolo :
@@ -134,7 +139,7 @@ def checkForSignal(data, m):
     preSell = np.full(m, np.nan)
     buyPrice = np.full(m, np.nan)
     sellPrice = np.full(m, np.nan)
-    BUY = np.full(m, 0) 
+    BUY = np.full(m, 0)
     SELL = np.full(m, 0)
     trend = np.full(m, 0)
     vxma_ = pd.Series(data['vxma'], dtype=np.float64)
@@ -147,7 +152,7 @@ def checkForSignal(data, m):
             elif vxma_[i] < vxma_[i-1] and vxma_[i-1] < vxma_[i-2] :
                 trend[i] = 0
             else:
-                trend[i] = trend[i-1] 
+                trend[i] = trend[i-1]
             #if trend change get pre-signal
             if trend[i] != 1 and trend[i-1] ==1 :
                 preBuy[i] = 0
@@ -160,14 +165,14 @@ def checkForSignal(data, m):
                 preSell[i] = 0
             #if close is above cloud is buy signal
             if Close[i] > vxma_[i] and (preBuy[i] == 1 or preBuy[i-1] == 1) and (BUY[i-1] !=1):
-                BUY[i] = 1 
+                BUY[i] = 1
                 buyPrice[i] = Close[i]
             elif Close[i] < vxma_[i] and (preSell[i] == 1 or preSell[i-1] == 1) and (SELL[i-1] != 1):
-                SELL[i] = 1 
+                SELL[i] = 1
                 sellPrice[i] = Close[i]
             else:
                 BUY[i] = 0
-                SELL[i] = 0 
+                SELL[i] = 0
         data['BUY'] = BUY
         data['buyPrice'] = buyPrice
         data['SELL'] = SELL
@@ -179,62 +184,64 @@ def checkForSignal(data, m):
 
 #Call everthing here if not benchmark
 def indicator(data, atr_input, atrM_input, ema_ip, subhag, smooth, rsi_ip, aol_ip, pivot):
-    m = len(data.index) 
-    try:
-        data = swingL(data, pivot)
-        data = swingH(data, pivot)
-        data['cBull'], data['cBear'] = andean(data['Close'], data['Open'], aol_ip, m)
-        data['alPha'] = alPha(data, atr_input, atrM_input, rsi_ip, m)
-        data['vxma'] = VXMA(data, ema_ip, subhag, smooth, m)
-        data = checkForSignal(data, m)
-        # Drop unwanted part
-        data.drop(['cBear','alPha','cBull'],axis=1,inplace=True)
-        return data
-    except Exception as e:
-        print(f'Indicator error : {e}')
-        return data
-    
+    m = len(data.index)
+#    try:
+    data = swingL(data, pivot)
+    data = swingH(data, pivot)
+    data['vxma'] = VXMA(data, ema_ip, subhag, smooth, atr_input, atrM_input, rsi_ip,aol_ip, m)
+    data = checkForSignal(data, m)
+    return data
+#    except Exception as e:
+#        print(f'Indicator error : {e}')
+#    return data
+
 
 #Trending a symbol.
 def benchmarking(data, atr_input, atrM_input, ema_ip, subhag, smooth, rsi_ip, aol_ip, pivot):
     tacollum = ['sma_200', 'macd', 'adx', 'adx+', 'adx-']
     datascoe = pd.DataFrame(columns=tacollum)
-    try:
-        datascoe['sma_200'] = ta.SMA(data['Close'],200)
-        datascoe['macd'], macdsignal, macdhist = ta.MACD(data['Close'],12,26,9)
-        datascoe['adx']  = ta.ADX(data['High'],data['Low'],data['Close'],14)
-        datascoe['adx+'] = ta.PLUS_DI(data['High'],data['Low'],data['Close'],14)
-        datascoe['adx-'] = ta.MINUS_DI(data['High'],data['Low'],data['Close'],14)
-        data = indicator(data, atr_input, atrM_input, ema_ip, subhag, smooth, rsi_ip, aol_ip, pivot)
-        m = len(datascoe.index)-1
+    #verify series
+    Close = pd.Series(data['Close'], dtype=np.float64)
+    Open = pd.Series(data['Open'], dtype=np.float64)
+    High = pd.Series(data['High'], dtype=np.float64)
+    Low = pd.Series(data['Low'], dtype=np.float64)
+    #try:
+    datascoe['sma_200'] = ta.SMA(Close,200)
+    datascoe['macd'], macdsignal, macdhist = ta.MACD(Open,12,26,9)
+    datascoe['adx']  = ta.ADX(High,Low,Close,14)
+    datascoe['adx+'] = ta.PLUS_DI(High,Low,Close,14)
+    datascoe['adx-'] = ta.MINUS_DI(High,Low,Close,14)
+    data = indicator(data, atr_input, atrM_input, ema_ip, subhag, smooth, rsi_ip, aol_ip, pivot)
+    m = len(datascoe.index)-1
+    #try:
+    adxx = 0
+    if float(datascoe['adx'][m]) > 25 and float(datascoe['adx+'][m]) > float(datascoe['adx-'][m]):
+        adxx = 10
+    elif float(datascoe['adx'][m]) > 25 and float(datascoe['adx+'][m]) < float(datascoe['adx-'][m]):
         adxx = 0
-        if float(datascoe['adx'][m]) > 25 and float(datascoe['adx+'][m]) > float(datascoe['adx-'][m]):
-            adxx = 10
-        elif float(datascoe['adx'][m]) > 25 and float(datascoe['adx+'][m]) < float(datascoe['adx-'][m]):
-            adxx = 0
-        else : adxx = 5
-        macd = (10 if float(datascoe['macd'][m]) > 0 else 0)
-        sma = (10 if float(datascoe['sma_200'][m]) < float(data['Close'][m]) else 0)
-        if data['vxma'][m] > data['vxma'][m-1]:
-            vxda = 10
-        elif data['vxma'][m] < data['vxma'][m-1]:
-            vxda = 0
-        else: vxda = 5
-        score = ((macd*MACD_W)/100 + (adxx*ADX_W)/100 + (sma*SMA200_W)/100  + (vxda*VXMA_W)/100)
-        if score > 8 :
-            scr = 'Extreme-Bullish'
-        elif score > 6 :
-            scr = 'Bullish'
-        elif score < 4 :
-            scr = 'Bearish'
-        elif score < 2 :
-            scr = 'Extreme-Bearish'
-        else:
-            scr = 'Side-Way'
-        return scr , data
-    except Exception as e:
-        print(f'Bencmarking is error : {e}')
-        return 'error' , data
+    else : adxx = 5
+    macd = (10 if float(datascoe['macd'][m]) > 0 else 0)
+    sma = (10 if float(datascoe['sma_200'][m]) < float(Close[m]) else 0)
+    if data['vxma'][m] > data['vxma'][m-1]:
+        vxda = 10
+    elif data['vxma'][m] < data['vxma'][m-1]:
+        vxda = 0
+    else: vxda = 5
+    score = ((macd*MACD_W)/100 + (adxx*ADX_W)/100 + (sma*SMA200_W)/100  + (vxda*VXMA_W)/100)
+    if score > 8 :
+        scr = 'Extreme-Bullish'
+    elif score > 6 :
+        scr = 'Bullish'
+    elif score < 4 :
+        scr = 'Bearish'
+    elif score < 2 :
+        scr = 'Extreme-Bearish'
+    else:
+        scr = 'Side-Way'
+    return scr , data
+    #except Exception as e:
+     #       print(f'Bencmarking is error : {e}')
+     #      return 'error' , data
 
 
 def ema(df, emafast, emaslow):
@@ -257,6 +264,9 @@ def ema(df, emafast, emaslow):
     except Exception as e:
         print(f'2EMA is error : {e}')
         return df
+
+
+
 
 
 
